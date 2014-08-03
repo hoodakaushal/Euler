adb5ed7a6ab7642d238f44a4e4fd9eac143f7faa
hoodakaushal <hoodakaushal@gmail.com>
2014-08-03 20:55:55 +0530

    Started problem 21.

--- solutions/021.py
+++ solutions/021.py
@@ -1,1 +1,22 @@
 __author__ = 'hooda'
+
+import tools
+
+
+def amicables(n):
+    ans = 0
+    sumdivs = {}
+    for i in range(1, n + 1):
+        sumdivs[i] = sum(tools.factors(i)[:-1])
+    print(sumdivs)
+    for i in range(1, len(sumdivs)):
+        j = sumdivs[i]
+        if not (j in sumdivs):
+            sumdivs[j] = sum(tools.factors(j)[:-1])
+        if (i == sumdivs[j]):
+            print(i, sumdivs[i])
+            ans += (i + sumdivs[i])
+    return ans
+
+
+print(amicables(10000))
