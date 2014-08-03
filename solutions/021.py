8c41ad4e1f70ef0076c39f808bb24561acee7575
hoodakaushal <hoodakaushal@gmail.com>
2014-08-03 21:13:07 +0530

    Finished problem 21.

--- solutions/021.py
+++ solutions/021.py
@@ -7,16 +7,17 @@ def amicables(n):
     ans = 0
     sumdivs = {}
     for i in range(1, n + 1):
-        sumdivs[i] = sum(tools.factors(i)[:-1])
+        sumdivs[i] = sum(tools.factors(i)) - i
     print(sumdivs)
     for i in range(1, len(sumdivs)):
         j = sumdivs[i]
-        if not (j in sumdivs):
-            sumdivs[j] = sum(tools.factors(j)[:-1])
-        if (i == sumdivs[j]):
-            print(i, sumdivs[i])
-            ans += (i + sumdivs[i])
-    return ans
+        if 1 <= j <= n:
+            a = i
+            b = sumdivs[i]
+            if sumdivs[b] == a and sumdivs[a] == b and not (a == b):
+                print(i, sumdivs[i])
+                ans += (i + sumdivs[i])
+    return ans // 2
 
 
 print(amicables(10000))
